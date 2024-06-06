12d802d18ba61c92c29e1958918913c0e85a4cd1
AlessioSalzillo <alessiosalzillo2107@icloud.com>
2024-06-06 11:06:43 +0200

    erqwf

--- src/main/java/com/acqualand/demo/model/Attrazione.java
+++ src/main/java/com/acqualand/demo/model/Attrazione.java
@@ -1,6 +1,10 @@
 package com.acqualand.demo.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,5 +25,4 @@ public class Attrazione {
     private boolean isOpen;
     private int minAge;
     private double minHeight;
-
 }
--- src/main/java/com/acqualand/demo/model/User.java
+++ src/main/java/com/acqualand/demo/model/User.java
@@ -21,6 +21,54 @@ public class User {
     private String name;
     private int age;
     private double height;
-    private float latitudine;
-    private float longitudine;
+    private double latitude;
+    private double longitude;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 }
